cadc323c7f748bc7b701e1a9e145495d00c8f39f
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-11-20 23:07:06 +0200

    auth...

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,9 +11,9 @@ export const Home = () => {
             <div className='absolute bg-black bg-opacity-40 inset-0'></div>
             <div className='relative text-white text-center'>
                 <h1 className='font-bold text-7xl'>Jewels</h1>
-                <p className='font-semibold text-center text-xl mt-5'>The best place to find your new best friend</p>
+                <p className='font-semibold text-center text-xl mt-5'>Timeless Elegance, Crafted to Shine</p>
                 <Link to='/products'>
-                    <button className='border-2 py-3 px-4 mt-16 hover:bg-slate-100 hover:text-black'>View Pets</button>
+                    <button className='border-2 py-3 px-4 mt-16 hover:bg-slate-100 hover:text-black'>View More</button>
                 </Link>
             </div>
       </div>
