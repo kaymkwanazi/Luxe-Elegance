69a7a39bc19662149c24657ab0b9afcf99478610
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-11-20 14:31:34 +0200

    3rd commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,14 +6,14 @@ export const Navbar = () => {
   return (
     <nav className='flex z-50 fixed w-full justify-between py-3 px-3 bg-slate-300'>
         <Link to='/'>
-            <h1 className='text-2xl'>PET SHOP</h1>
+            <h1 className='text-2xl'> PET SHOP</h1>
         </Link>
         <ul className='flex items-center justify-center'>
-            <li className='space-x-5'>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/about">Products</a>
-                <a href="/products">Contact</a>
+            <li className='font-semibold space-x-5'>
+                <a href="/" className='hover:text-green-500'>Home</a>
+                <a href="/about" className='hover:text-green-500'>About Us</a>
+                <a href="/about" className='hover:text-green-500'>Products</a>
+                <a href="/products" className='hover:text-green-500'>Contact Us</a>
             </li>
         </ul>
         <button className='bg-green-500 hover:bg-green-700 text-white font-bold px-4 rounded'>Sign In</button>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import pic1 from '../images/pic2.jpg'
+import aboutPic from '../images/aboutPic.jpg'
 import { Link } from 'react-router-dom'
 
 export const Home = () => {
@@ -8,13 +9,20 @@ export const Home = () => {
       <div className='h-screen bg-cover bg-center relative flex flex-col items-center justify-center' 
         style={{backgroundImage: `url(${pic1})`}}>
             <div className='absolute bg-black bg-opacity-40 inset-0'></div>
-            <div className='relative text-white'>
+            <div className='relative text-white text-center'>
                 <h1 className='font-bold text-7xl'>THE PET SHOP</h1>
                 <p className='font-semibold text-center text-xl mt-5'>The best place to find your new best friend</p>
                 <Link to='/products'>
-                    <button className='border-2 py-3 px-4 mt-16 ml-36 hover:bg-slate-100 hover:text-black'>View Pets</button>
+                    <button className='border-2 py-3 px-4 mt-16 hover:bg-slate-100 hover:text-black'>View Pets</button>
                 </Link>
             </div>
+      </div>
+      <div className='container mx-auto px-4 my-10'>
+        <div className='grid grid-cols-1 md:grid-cols-2'>
+            <div className='overflow-hidden'>
+                <img src={aboutPic} alt='about' className='w-full h-full object-cover' style={{ objectFit: 'cover', height: '100%' }} />
+            </div>
+        </div>
       </div>
     </>
   )
