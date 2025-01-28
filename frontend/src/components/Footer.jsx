673ff6feba7decb2bd57d71d0c397fca4e84b0c0
TerryB <terryb@innoble.co.za>
2025-01-28 14:59:56 +0200

    about section on homepage done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,12 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { FaFacebook, FaFacebookF, FaInstagram, FaInstagramSquare, FaLinkedin, FaLinkedinIn, FaPinterest, FaPinterestP, FaWhatsapp, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className='fixed md:px-10 right-0 top-1/2 w-16 h-auto transform -translate-y-1/2  flex flex-col justify-end items-center'>
-      <div className='gap-10 flex flex-col items-center md:mr-10'>
-          <hr className='border-t border-[#f2b543] w-32 transform rotate-90 mb-16 mt-10' />
-          <FaFacebookF size={32} className='text-slate-100' />
-          <FaPinterestP size={32} className='text-slate-100 '/>
-          <FaInstagram size={32} className='text-slate-100 '/>
-          <FaLinkedinIn size={32} className='text-slate-100 '/>
-          <hr className='border-t border-[#f2b543] w-32 transform rotate-90 mt-16' />
-      </div>
-    </div>
+   <>
+   
+   </>
   );
 };
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import pic1 from '../images/hand with gold ring.jpg'
 // import aboutPic from '../images/aboutPic.jpg'
@@ -7,6 +8,8 @@ import earrings from '../images/earrings-category.jpg'
 import bracelets from  '../images/bracelet-category.png'
 import necklaces from '../images/necklace-category.jpg'
 import watches from '../images/women-watch.jpg'
+import aboutPic from '../images/aboutPic-1.jpg'
+import aboutPic2 from '../images/about2.webp'
 
 const categories = [
   {name: 'Earrings', image: earrings, description: 'From subtle studs to statement pieces, perfect for any occasion'},
@@ -31,8 +34,8 @@ const Home = () => {
             </div>
       </div>
 
-      {/* Mid section */}
-      <div className='bg-[#494949]  text-center py-10'>
+      {/* Mid shop section */}
+      <div className='bg-[#494949]  text-center pt-10 pb-16'>
         <h1 className='text-4xl text-white'>Shop by Category</h1>
         <hr className='mx-auto my-4 w-10 border-t-2 border-[#FFD700] pb-10'></hr>
         <div className='container mx-auto px-4 grid cols-1 md:grid-cols-4 gap-5'>
@@ -49,6 +52,29 @@ const Home = () => {
          </div>
 
       </div>
+
+      {/* Mid about Section */}
+      <div className='relative bg-[#DBDBDB] text-center py-10'>
+        <div className='container mx-auto px-4 flex justify-between space-x-36'>
+        <div className='relative'>
+            <img src={aboutPic} className=' w-full object-cover rounded-md border-white shadow-md'></img>
+          </div>
+          <div className='absolute top-16 left-56'>
+            <img src={aboutPic2} alt='overlay' className='rotate-90 w-full h-80 object-cover rounded-md border-2 border-white shadow-md '></img>
+          </div>
+          <div>
+            <h1 className='text-2xl md:text-6xl text-black font-carattere pb-5'>Discover the timeless beauty of fine jewellery at Luxe Elegance.</h1>
+            <p className='text-lg text-black text-center'>Our exquisite designs are crafted to celebrate life`s most precious moments, blending sophistication with modern artistry. Let every piece tell your unique story of elegance and charm.</p>
+            <Link>
+              <button className='border-2 border-black py-3 px-4 mt-10  hover:bg-slate-100 hover:text-black'>Learn More</button>
+            </Link>
+         
+          </div>
+
+        </div>
+       
+      </div>
+
       
     </>
   )
