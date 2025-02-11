5adb3674a62b6be7e72e6fcd1dffff5d7e6d851b
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2025-02-11 15:40:34 +0200

    edits

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,6 +9,7 @@ import instagram from '../images/instagram.png'
 import twitter from '../images/twitter.png'
 import pinterest from '../images/pinterest.png'
 import youtube from '../images/youtube.png'
+import { IoIosArrowDropup } from 'react-icons/io';
 
 
 const Footer = () => {
@@ -17,54 +18,35 @@ const Footer = () => {
   }
   return (
    <>
-   <div className='bg-[#515151] border-t-2 border-t-yellow-400 py-5'>
-    <div className='container mx-auto px-4 grid cols-1 md:grid-cols-2'>
-      <div className='flex gap-16'>
-        <div>
-          <img src={logo} width={62} className='rounded-full'></img>
-        </div>        
-          <div className='flex flex-col gap-5'>
-            <h2 className='text-white font-semibold text-xl text-center'>Follow us</h2>
-              <div className='flex  gap-5'>
-                <img src={facebook} alt="Facebook" width={30} className='cursor-pointer'/>
-                <img src={instagram} alt="Instagram" width={30} className='cursor-pointer'/>
-                <img src={twitter} alt="Twitter" width={30} className='cursor-pointer'/>
-                <img src={pinterest} alt="Pinterest" width={30} className='cursor-pointer'/>
-              
+    <div className='bg-[#515151] border-t-2 border-t-yellow-400 py-5'>
+      <div className='container mx-auto px-4 flex justify-between items-center'>      
+          <div>
+            <img src={logo} width={62} className='rounded-full'></img>
+          </div>        
+            <div className='flex flex-col gap-5'>
+              <h2 className='text-white font-semibold text-xl text-center'>Follow us</h2>
+                <div className='flex  gap-5'>
+                  <img src={facebook} alt="Facebook" width={30} className='cursor-pointer'/>
+                  <img src={instagram} alt="Instagram" width={30} className='cursor-pointer'/>
+                  <img src={twitter} alt="Twitter" width={30} className='cursor-pointer'/>
+                  <img src={pinterest} alt="Pinterest" width={30} className='cursor-pointer'/>
+                
+                </div>
             </div>
-          </div>
+          
+          <div className='flex justify-center'>
+            <button 
+              onClick={scrollToTop} 
+              className='text-white p-2  transition duration-300'
+              aria-label="Scroll to top"
+            >
+                <IoIosArrowDropup size={36}/>
+            </button>
+          </div>       
+        
       </div>
-     
-      <div className='flex flex-col space-y-5'>
-        {/* <div className='flex mb-10'>
-          <ul className="lg:flex lg:justify-center lg:items-center lg:space-x-16 w-full">
-              <li className="block mt-4 lg:mt-0 text-white hover:font-bold">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="block mt-4 lg:mt-0 text-white hover:font-bold">
-                <Link to="/products">Shop</Link>
-              </li>
-              <li className="block mt-4 lg:mt-0 text-white hover:font-bold">
-                <Link to="/about">About</Link>
-              </li>
-              <li className="block mt-4 lg:mt-0 text-white hover:font-bold">
-                <Link to="/contact">Contact us</Link>
-              </li>
-          </ul>
-        </div> */}
-        <div className='flex justify-center'>
-          <button 
-            onClick={scrollToTop} 
-            className='text-white p-2  transition duration-300'
-            aria-label="Scroll to top"
-          >
-               Back to top
-          </button>
-        </div>       
-      </div>
+      <p className='text-white text-sm text-center mt-16'><FaRegCopyright className='inline size-3'/> Luxe Elegance. All rights reserved.</p>   
     </div>
-    <p className='text-white text-center mt-10'><FaRegCopyright className='inline size-3'/> Luxe Elegance. All rights reserved.</p>   
-   </div>
    </>
   );
 };
